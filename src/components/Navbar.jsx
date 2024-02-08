cb6953b94fc1b0e19c409d127707146dd7ed5428
Mynul Islam <mynul.sakil@gmail.com>
2024-02-08 09:03:47 +0600

    initial setup finished

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="navbar bg-base-100">
+      <a className="btn btn-ghost text-xl">TODO LIST</a>
+    </div>
+  );
+}
